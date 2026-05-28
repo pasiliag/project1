46c1226e9d158c7fac22abfd9f745c1f542a5b19
pasiliag <pasiliag@users.noreply.github.com>
2026-05-28 16:09:05 +0800

    Add GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? "/project1" : "",
+  assetPrefix: isGithubActions ? "/project1/" : "",
+};
 
 module.exports = nextConfig;
